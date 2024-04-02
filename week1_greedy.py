0824a5aa6fccf1c9ab60676b81171c8c9e921104
june-choi <choi_oi7@yonsei.ac.kr>
2024-04-02 16:54:38 +0900

    week1_late_submission

---
+++ week1_greedy.py
@@ -0,0 +1,16 @@
+import sys
+
+#input 여러 줄 입력 받기
+n = int(sys.stdin.readline())
+arr = [sys.stdin.readline().strip() for i in range(n)]
+
+#문제풀이
+for i in range(n):
+    amount = int(arr[i])
+    coins = [25, 10, 5, 1]
+    coin_count = []
+    for coin in coins:
+        count = amount // coin
+        coin_count.append(count)
+        amount %= coin
+    print(*coin_count)
